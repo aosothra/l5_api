57f4d5d549e615505a34407ad2330451a2e78119
aosothra <arhdam@yandex.ru>
2022-03-13 21:23:28 +0400

    Function changed for better readability

--- predict_salary.py
+++ predict_salary.py
@@ -1,11 +1,10 @@
 def predict_rub_salary(currency, low_limit, high_limit):
-    salary = None
-    if currency.lower() in {'rub', 'rur'}:
-        if low_limit and high_limit:
-            salary = (int(high_limit) + int(low_limit))/2
-        elif low_limit:
-            salary = int(low_limit)*1.2
-        elif high_limit:
-            salary = int(high_limit)*0.8
+    if not currency.lower() in {'rub', 'rur'}:
+        return None
 
-    return salary
+    if low_limit and high_limit:
+        return (int(high_limit) + int(low_limit))/2
+    elif low_limit:
+        return int(low_limit)*1.2
+    elif high_limit:
+        return int(high_limit)*0.8
